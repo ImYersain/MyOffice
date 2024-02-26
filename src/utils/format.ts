b53e1e352ba5ef8e7b258dd1056d45958c0bf470
yersain.aldabayev <>
2024-02-26 12:46:19 +0100

    Updates

--- src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-import {format, parse} from 'date-fns';
-
-export const normalizeDate = (dateString: string) => {
-  const parsedDate = parse(dateString, 'EEE, dd MMM yyyy HH:mm:ss xx', new Date());
-  const normalizedDate = format(parsedDate, 'yyyy-MM-dd');
-  return normalizedDate;
-};
